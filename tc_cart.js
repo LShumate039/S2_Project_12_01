24be1cfece6d46870d618b75557dfb2472b7dc66
Unknown <LShumate039@west-mec.org>
2019-02-28 18:20:26 -0700

    Final commit
    ready to be graded

---
+++ tc_cart.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+
+   Author: 
+   Date:   
+   
+   Filename: tc_cart.js
+	
+*/
+var orderTotal = 0;
+
+var cartHTML = "<table><tr><th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th></tr>";
+//this is the math part and it adds up all of the valus  
+for (var i = 0; i < item.length; i++) {
+    cartHTML += "<tr> <td><img src='tc_" + item[i] + ".png' alt='item'/></td>";
+    cartHTML += "<td>" + itemDescription[i] + "</td> <td>$" + itemPrice[i] + "</td> <td>" + itemQty[i] + "</td>";
+    var itemCost = itemPrice[i] * itemQty[i];
+    cartHTML += "<td>$" + itemCost + "</td>" + "</tr>";
+    orderTotal = orderTotal + itemCost;
+}
+
+cartHTML += "<tr> <td colspan='4'>Subtotal</td> <td>$" + orderTotal + "</td> </tr> </table>";
+//this puts the code inside the div inside the html 
+document.getElementById("cart").innerHTML = cartHTML;
